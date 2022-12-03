3fe664ace6c99fb9430484adbef2553f17391708
Rachel Panico <rachelpanico@Rachels-Air.home>
2022-12-03 10:17:59 -0500

    hack or snooze

---
+++ js/nav.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".nav-link").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navProfile.text(`${currentUser.username}`).show();
+}
+
+function navSubmitStoryClick(e){
+  e.preventDefault();
+  hidePageComponents();
+  $allStoriesList.show();
+  $submitForm.show();
+}
+
+$navSubmitStory.on("click", navSubmitStoryClick);
+
+
+function navMyStories(e){
+  hidePageComponents();
+  putOwnStoriesOnPage();
+  $ownStories.show();
+}
+
+$body.on("click", "#nav-my-stories", navMyStories);
+
+function navFavesClick(e){
+hidePageComponents();
+putFavesOnPage();
+}
+
+$body.on("click", "#nav-faves", navFavesClick);
+
+function navProfileClick(e){
+  hidePageComponents();
+  $profile.show();
+}
+
+$navProfile.on("click", navProfileClick);
---
+++ js/stories.js
@@ -0,0 +1,168 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+
+function generateStoryMarkup(story, deleteBtn = false) {
+  //console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  let star = Boolean(currentUser);
+
+  console.log("Delete button:" + deleteBtn);
+  console.log("Star: " + star)
+
+  return $(`
+      <li id="${story.storyId}">
+        <span>${deleteBtn ? makeDeleteBtn() : ""}</span>
+        <span>${star ? makeStar(story, currentUser) : ""}</span>
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+function putFavesOnPage(){
+  $faveStories.empty();
+
+  if(currentUser.favorites.length === 0){
+    $faveStories.append("<div>No favorites found</div>");
+  }
+  else{
+    for(let story of currentUser.favorites){
+      let $story = generateStoryMarkup(story);
+      $faveStories.append($story);
+    }
+  }
+  $faveStories.show();
+}
+
+
+
+function makeDeleteBtn(){
+  return `
+    <span class="trash">
+    <i class="fa-solid fa-trash-xmark"></i>
+    </span>
+  `
+}
+
+function makeStar(story, user){
+let isFave = user.isFave(story);
+let star = isFave ? "solid" : "regular";
+
+return `
+  <span class="star">
+   <i class="fa-${star} fa-star></i> 
+  </span>`;
+}
+
+async function deleteStory(e){
+
+let targetId = $(e.target).closest("li").attr("id");
+
+await storyList.removeStory(user, targetId);
+
+putOwnStoriesOnPage();
+
+}
+
+$ownStories.on("click", ".trash", deleteStory);
+
+
+async function submitNewStory(e){
+
+  e.preventDefault();
+
+  let title = $("#title-input").val();
+  let author = $("#author-input").val();
+  let url = $("#url-input").val();
+  let username = currentUser.username;
+  let storyData = { title, author, url, username };
+
+  console.log("Inside submitNewStory function");
+
+  let story = await storyList.addStory(currentUser, storyData);
+
+  let $story = generateStoryMarkup(story);
+
+  $allStoriesList.prepend($story);
+
+  $submitForm.hide();
+  $submitForm.trigger("reset");
+
+}
+
+$submitForm.on("submit", submitNewStory);
+
+function putOwnStoriesOnPage(){
+  $ownStories.empty();
+
+  if(currentUser.ownStories.length === 0){
+    $ownStories.append("<p>No stories found</p>");
+  }
+  else{
+    for(let story of currentUser.ownStories){
+      let $story = generateStoryMarkup(story, true);
+      $ownStories.append($story);
+    }
+  }
+  $ownStories.show();
+}
+
+
+
+
+async function makeFaves(e){
+  let $target = $(e.target);
+  let targetId = $target.closest("li").attr("id");
+  let story = storyList.stories.find(s => s.targetId === targetId);
+
+  if($target.hasClass("fa-solid")){
+    await currentUser.removeFave(story);
+    $target.closest("i").toggleClass("fa-solid fa-regular");
+  }
+  else{
+    await currentUser.addFave(story);
+    $target.closest("i").toggleClass("fa-solid fa-regular");
+  }
+}
+
+$storiesLists.on("click", ".fa-star", makeFaves);
